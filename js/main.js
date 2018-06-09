43427abfc9acc9ca70e02e48bc25c5ad0ef962f9
Christopher Martin <martintechsolution@gmail.com>
2018-06-09 16:45:03 -0400

    Made accessibility changes

--- js/main.js
+++ js/main.js
@@ -163,7 +163,7 @@ createRestaurantHTML = (restaurant) => {
   pictureText.innerHTML = image.alt;
   li.append(pictureText);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
